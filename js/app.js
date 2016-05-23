ed2d5fb20465f56720f8b792b65d130512abafda
eliteofindia <eliteofindiadream@gmail.com>
2016-05-23 17:05:23 +0800

    adding time component to index.html footer

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 'use strict';
 
 angular.module('myApp', [])
-  .controller('MovieController', function($scope, $http){
+  .controller('MovieController', function($scope, $http, $interval){
     $scope.$watch('search', function() {
       fetch();
     });
+	
+	$scope.theTime = new Date().toLocaleTimeString();
+	  $interval(function () {
+		  $scope.theTime = new Date().toLocaleTimeString();
+	  }, 1000);
 
     $scope.search = "Sherlock Holmes";
-
     function fetch(){
-      $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      $http.get("http://www.omdbapi.com/?t=" + $scope.search +"&tomatoes=true&plot=full")
       .then(function(response){ $scope.details = response.data; });
 
       $http.get("http://www.omdbapi.com/?s=" + $scope.search)
@@ -17,7 +21,7 @@ angular.module('myApp', [])
     }
 
     $scope.update = function(movie){
-      $scope.search = movie.Title;
+      $scope.search = movie.Title;	
     };
 
     $scope.select = function(){
