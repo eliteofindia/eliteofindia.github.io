d8cb0277e01d9fcb8c3ffe839fb15f1f69bb6b83
eliteofindia <eliteofindiadream@gmail.com>
2016-06-08 16:30:30 +0800

    adding hits feature

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('myApp', [])
-  .controller('MovieController', function($scope, $http, $interval){
+var app = angular.module('myApp', []);
+
+  app.controller('MovieController',function($scope, $http, $interval){
     $scope.$watch('search', function() {
       fetch();
     });
@@ -10,14 +11,18 @@ angular.module('myApp', [])
 	  $interval(function () {
 		  $scope.theTime = new Date().toLocaleTimeString();
 	  }, 1000);
-
+	  
     $scope.search = "Sherlock Holmes";
     function fetch(){
-      $http.get("http://www.omdbapi.com/?t=" + $scope.search +"&tomatoes=true&plot=full")
+      $http.get("http://www.omdbapi.com/?t=" + $scope.search +"&tomatoes=true&plot=full", {cache: false})
       .then(function(response){ $scope.details = response.data; });
 
-      $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+      $http.get("http://www.omdbapi.com/?s=" + $scope.search, {cache: false})
       .then(function(response){ $scope.related = response.data; });
+	  
+	  $http.get("http://exp-expservices.rhcloud.com/hits", {cache: false})
+      .then(function(response){ $scope.hits = response.data; }, function(response){console.log(response);});
+
     }
 
     $scope.update = function(movie){
@@ -28,3 +33,4 @@ angular.module('myApp', [])
       this.setSelectionRange(0, this.value.length);
     }
   });
+  
