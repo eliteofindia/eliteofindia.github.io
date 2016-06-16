19736b1213f7ce09a3a6f80d2a70144f6679f3ed
eliteofindia <eliteofindiadream@gmail.com>
2016-06-16 14:09:54 +0800

    pure angular implementation submitted

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 'use strict';
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "MovieController"})
+    // Pages
+	.when("/surfer", {templateUrl: "partials/surfer.html", controller: "MovieController"})
+    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
+    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
+    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
+    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
+    // Blog
+    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
 
   app.controller('MovieController',function($scope, $http, $interval){
     $scope.$watch('search', function() {
